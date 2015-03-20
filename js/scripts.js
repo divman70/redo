1aa5c5b12b0e2e02ac21cfc38bc4f5d66856637d
divman70 <dkoch@eagles.ewu.edu>
2015-03-20 00:50:50 -0700

    fixed all styling and added responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,26 @@
-$('.warnning').hide();
+$('.a').hide();
+$('.b').hide();
+$('.c').hide();
+$('.d').hide();
 
-$('.button').click(function(){
+$(window).on('resize', function(){
+      var item = $(this);
+      if (item.width() > 992) { 
+
+      $('#main, #input, #nav').addClass('computer only').removeClass('mobile only, tablet only');
+
+      }
+    else if (item.width() < 480)
+    {
+        $('#main, #input, #nav').addClass('mobile only').removeClass('computer only, tablet only');
+    }
+      else 
+    {
+        $('#main, #input, #nav').addClass('tablet only').removeClass('computer only, mobile only');
+    }
+});
+
+$('.get').click(function(){
   
   var zip = $('#zip').val();
 
@@ -21,6 +41,7 @@ success: function(weather){
       // get and store current weather code
       var type = weather.code;
 
+      var title = "WEATHER"
       // var type =  'img\\' + weather.code + '.png';
       // output weather code
       console.log(type);
@@ -33,39 +54,14 @@ success: function(weather){
       $('.city').text(city);
       $('.state').text(state);
       $('.currently').text(currentTemp);
+      $('.today').text(title);
       // $('.img').src.replace(type);
-
+      $('.a').show();
+      $('.b').show();
+      $('.c').show();
+      $('.d').show();
       // $('p').text(upcomingWeather); 
 
-      // checks for dangerous weather conditions 
-      if (type == 0 || type == 1 || type == 2 || type == 3 || type == 4) {
-        $('body').css('background-image', 'url("img/storm.jpg")');
-        $('.warnning').show();
-        }
-         // checks for rainy weather conditions 
-        else if (type == 8 || type == 9 || type == 10 || type == 11 || type == 29 || type == 30) {
-          $('body').css('background-image', 'url("img/rain.jpg")');
-        }
-        // checks for snowy weather conditions
-        else if (type == 13 || type == 14 || type == 15 || type == 16 || type == 41 || type == 43 || type == 46 || temp <= 36) {
-          $('body').css('background-image', 'url("img/ice.jpg")');
-        }
-        // checks for sunny weather conditions
-        else if (type == 31 || type == 32 || type == 34 || type == 35 || type == 36) {
-          $('body').css('background-image', 'url("img/sun.jpg")');
-        }
-        // checks for foggy weather conditions
-        else if (type == 20) {
-          $('body').css('background-image', 'url("img/fog.jpg")');
-        }
-        // checks for night & stormy weather conditions
-         else if (type == 26 || type == 27 || type == 29 || type == 33 || type == 38 || type == 39 || type == 40 || type == 45 || type == 47) {
-          $('body').css('background-image', 'url("img/night.jpg")');
-        }
-        // goes to default image
-        else {
-          $('body').css('background-image', 'url("img/road.jpg")');
-        }
 },
 
 error: function(message){
@@ -75,61 +71,143 @@ error: function(message){
 
 });
 
-// if (navigator.geolocation) {
-//     // Yes! Show button
-//     $('.getGeolocation').show(); 
-//   } else {
-//     // No. Hide button
-//     $('.getGeolocation').hide();
-//   }
+if (navigator.geolocation) {
+    // Yes! Show button
+    $('.getGeolocation').show(); 
+  } else {
+    // No. Hide button
+    $('.getGeolocation').hide();
+  }
 
 
-//   $('.getGeolocation').on('click', function() {
+  $('.getGeolocation').on('click', function() {
   
-//     navigator.geolocation.getCurrentPosition(function(position) {
-//     //load weather using your lat/lng coordinates. See _loadWeather()_ below
-//     loadWeather(position.coords.latitude+','+position.coords.longitude); 
-//     // See latitute & longitude. Note, wait a few seconds
-//     console.log(position.coords.latitude+','+position.coords.longitude);
-//   });
+    navigator.geolocation.getCurrentPosition(function(position) {
+    //load weather using your lat/lng coordinates. See _loadWeather()_ below
+    loadWeather(position.coords.latitude+','+position.coords.longitude); 
+    // See latitute & longitude. Note, wait a few seconds
+    console.log(position.coords.latitude+','+position.coords.longitude);
+  });
   
-// });
+});
 
-//   var loadWeather = function(location) {
+  var loadWeather = function(location) {
     
-//     $.simpleWeather({
-//     location: location,
+    $.simpleWeather({
+    location: location,
     
-//     // Get _weather_ object
-//     success: function(weather) {
+    // Get _weather_ object
+    success: function(weather) {
         
-//       console.log(weather);
+      console.log(weather);
       
-//       // Get & store temperature
-//       var temp = weather.temp;
-//       // Get & store city
-//       var city = weather.city;
+      // Get & store temperature
+      var temp = weather.temp;
+      // Get & store city
+      var city = weather.city;
 
-//       var state = weather.region;
+      var state = weather.region;
 
-//       var currentTemp = weather.currently;
+      var currentTemp = weather.currently;
 
+      var title = "WEATHER"
             
-//       // Output to hooks in HTML
-//       $('.temp').text(temp);
-//       $('.city').text(city);
-//       $('.state').text(state);
-//       $('.currently').text(currentTemp);
+      // Output to hooks in HTML
+      $('.temp').text(temp);
+      $('.city').text(city);
+      $('.state').text(state);
+      $('.currently').text(currentTemp);
+      $('.today').text(title);
+
+
+      $('.a').show();
+      $('.b').show();
+      $('.c').show();
+      $('.d').show();
       
       
-//       // See console for _weather_ object
-//       console.log(weather);
-//     }
+      // See console for _weather_ object
+      console.log(weather);
+    }
   
-//   });
+  });
     
-// }; // end of _loadWeather()_ function
+}; // end of _loadWeather()_ function
 
+$('.tomorrow').click(function(){
+  
+  var zip = $('#zip').val();
 
+  $.simpleWeather ({
+    location: zip,
+    unit: 'f',
+
+// Get _weather_ object
+success: function(weather){
+
+      var state = weather.forecast[1].text
+      
+      var currentTemp = weather.forecast[1].high+'°'
+  
+      var next = weather.forecast[1].day
+
+      var title = "WEATHER"
+
+      // var type =  'img\\' + weather.code + '.png';
+      // output weather code
+      console.log(next);
+
+      // get and store weather for next day
+      // var upcomingWeather = weather.forecast[1].day + ' ' + weather.forecast[1].high;
+      
+      // Output to hooks in HTML
+      $('.temp').text(currentTemp);
+      $('.state').text(state);
+      $('.currently').text(next);
+      $('.today').text(title);
+     
+      $('.a').show();
+      $('.b').show();
+      $('.c').hide();
+      $('.d').show();
+},
+
+error: function(message){
+  $('body').html('<p>' + error + '</p>');
+}
+      });
+
+});
   
 
+$('.it').click(function(){
+  
+  var zip = $('#zip').val();
+
+  $.simpleWeather ({
+    location: zip,
+    unit: 'f',
+
+// Get _weather_ object
+success: function(weather){
+
+      var state = weather.updated
+  
+      console.log(state);
+
+      $('.today').text(state)
+      
+     
+      $('.a').hide();
+      $('.b').hide();
+      $('.c').hide();
+      $('.d').hide();
+},
+
+error: function(message){
+  $('body').html('<p>' + error + '</p>');
+}
+      });
+
+});
+  
